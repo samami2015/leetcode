2fcc83f17c01b6217cc404f07dfafce4ccc2c1b1
wangsen <22232647@qq.com>
2021-07-23 23:58:57 +0800

    university week 10

--- src/LC0105.java
+++ src/LC0105.java
@@ -43,6 +43,33 @@ public class LC0105 {
         return root;
     }*/
 
+/*    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < inorder.length; i++) {
+            map.put(inorder[i], i);
+        }
+        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length, map);
+    }
+
+    private TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end, int[] inorder, int i_start, int i_end, HashMap<Integer, Integer> map) {
+        if (p_start == p_end) {
+            return null;
+        }
+        int root_val = preorder[p_start];
+        TreeNode root = new TreeNode(root_val);
+        int i_root_index = map.get(root_val);
+*//*        for (int i = i_start; i < i_end; i++) {
+            if (root_val == inorder[i]) {
+                i_root_index = i;
+                break;
+            }
+        }*//*
+        int leftNum = i_root_index - i_start;
+        root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index, map);
+        root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end, map);
+        return root;
+    }*/
+
     public TreeNode buildTree(int[] preorder, int[] inorder) {
         HashMap<Integer, Integer> map = new HashMap<>();
         for (int i = 0; i < inorder.length; i++) {
@@ -58,12 +85,6 @@ public class LC0105 {
         int root_val = preorder[p_start];
         TreeNode root = new TreeNode(root_val);
         int i_root_index = map.get(root_val);
-/*        for (int i = i_start; i < i_end; i++) {
-            if (root_val == inorder[i]) {
-                i_root_index = i;
-                break;
-            }
-        }*/
         int leftNum = i_root_index - i_start;
         root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index, map);
         root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end, map);
